f457a680bc9bae63e1a82c981b22f56c9fef9728
tjpreyear <tyrese_preyear@yahoo.com>
2020-05-28 03:57:42 -0500

    Added a Web request model to check for errors when trying to access the specif site

--- Data/Web_Request.py
+++ Data/Web_Request.py
@@ -1,22 +1,23 @@
 import requests
 
-class GetWebsite:
+class CheckWebRequest:
 
-	def __init__(self, website, searchedWord):
-		self.completedWebsiteName = website + searchedWord
+	def __init__(self):
+		self.completedWebsite = ""
 
-	@property
+
 	def connectToWebsite(self):
 		try:
-			self.makeCall = requests.get(self.completedWebsiteName, timeout=3.0)
-			self.makeCall.raise_for_status()
+			self.makeCall = requests.get(self.completedWebsite, timeout=3.0)
+			print(self.makeCall.raise_for_status())
 		except requests.exceptions.Timeout as errt:
 			return errt  # "Timed Out"
 		except requests.exceptions.ConnectionError as errc:
-			return errc  # "Connection Error"
+			return errc   # "Connection Error"
 		except requests.exceptions.HTTPError as errh:
 			return errh  # "HTTP Error"
 		except requests.exceptions.RequestException as err:
 			return err  # "Something is wrong"
 		else:
 			return self.makeCall
+
