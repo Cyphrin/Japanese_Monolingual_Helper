a14b27694496138802d1263553ee690aa03cc4ec
tjpreyear <tyrese_preyear@yahoo.com>
2020-06-18 01:33:55 -0500

    Created function that compared a list of known words to the list that is being generated and displayed it in the display box.
    Fixed issue od data only showing in one box at a time.

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from View.GUI_View import View
 from Data.Thesaurus import Thesaurus
+from Data.Read_Files import ReadFiles
 
 try:
 	import tkinter as tk
@@ -10,8 +11,10 @@ except ImportError:  # python 2
 class Controller(tk.Listbox):
 	data = Thesaurus()
 
+
 	def __init__(self):
 		self.view = View(self)
+
 
 	# Function makes call to Gui_View to start the mainloop() to show the GUI.
 	def main(self):
@@ -19,6 +22,7 @@ class Controller(tk.Listbox):
 
 	def onButtonClick(self):
 		self.resultsBox()
+		self.comparedWordsList()
 
 	# This function makes a call From Data.Thesaurus to the Thesaurus class to
 	# input the web tags and the specific site to retrieve the data for synonyms.
@@ -40,14 +44,23 @@ class Controller(tk.Listbox):
 
 	# This function is displaying the data that is retrieved from functions getSynonym and getAntonym.
 	def resultsBox(self):
+
 		if self.view.rbValue.get() == 1:
 			self.getSynonym()
+			self.view.displayResultsBox(self.view.tab1)
 			self.view.showResults(self.data.finalList)
 
 		else:
 			self.getAntonym()
+			self.view.displayResultsBox(self.view.tab1)
 			self.view.showResults(self.data.finalList)
 
+	# Function compares the Words you already know in your list with words that are generated
+	def comparedWordsList(self,):
+		readFiles = ReadFiles()
+		readFiles.compareFiles(self.data.finalList)
+		self.view.displayResultsBox(self.view.tab2)
+		self.view.showResults(readFiles.turnToList)
 
 if __name__ == '__main__':
 	monolingualApp = Controller()
